379c45740c137ed9bee1b2ca4ca2368cca943828
Steve Dakh <slavik0329@gmail.com>
2020-07-04 19:10:56 -0400

    Updates

---
+++ precache-manifest.08646199c7b1f090f8db07b8bdaaa565.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d973aa426d431be88af5821dd819a9db",
+    "url": "/sailplane-web/index.html"
+  },
+  {
+    "revision": "2f833cd84ed9798a6007",
+    "url": "/sailplane-web/static/css/main.aed6dde3.chunk.css"
+  },
+  {
+    "revision": "ebac73503a0f82a6d78f",
+    "url": "/sailplane-web/static/js/2.c8cb83c4.chunk.js"
+  },
+  {
+    "revision": "d98a526d32c334d44857bafbb4dc6daa",
+    "url": "/sailplane-web/static/js/2.c8cb83c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f833cd84ed9798a6007",
+    "url": "/sailplane-web/static/js/main.541cddd7.chunk.js"
+  },
+  {
+    "revision": "b6a146d45d49a419cc90",
+    "url": "/sailplane-web/static/js/runtime-main.a6747d37.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sailplane-web/precache-manifest.b9ffec7c7b08487ada9bc6fdce871215.js"
+  "/sailplane-web/precache-manifest.08646199c7b1f090f8db07b8bdaaa565.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sailplane-web/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
