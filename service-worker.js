f30a414c89ad12a9410d2077bea202f46958ee78
Steve Dakh <slavik0329@gmail.com>
2020-07-20 21:20:17 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sailplane-web/precache-manifest.49b8ffba7f4bd73c09b533661139f105.js"
+  "/sailplane-web/precache-manifest.dcb0d19e0bdeeb89065c96e7d35f970a.js"
 );
 
 self.addEventListener('message', (event) => {
